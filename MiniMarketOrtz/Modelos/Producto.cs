a7ba6e06b6d2cc3a626a8e989dd74d7ed5d78516
Laboratorio <Laboratorio@QRC052PCLU5020>
2026-04-21 12:33:24 -0400

    AAAAAAAAAAAAA

---
+++ MiniMarketOrtz/Modelos/Producto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniMarketOrtz.Modelos
+{
+    internal class Producto
+    {
+
+        private int id { get; set; }
+        private string nombre { get; set; }
+
+        private 
+
+        private double precio { get; set; }
+
+
+    }
+}
